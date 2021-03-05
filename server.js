7373ca900fbc344e3346bb70043b44baa3b68005
olig331 <ohg3@hotmail.co.uk>
2021-03-05 10:13:04 +0000

    deploy

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ io.on("connection", socket => {
 
 if(process.env.PROD) {
     app.use(express.static(path.join(__dirname, "./client/build")));
-    app.get('*', (req, res) => {
+    app.get('/', (req, res) => {
         res.sendFile(path.join(__dirname, "./client/build/index.html"));
     });
 }
