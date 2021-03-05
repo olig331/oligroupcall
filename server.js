63afa87336e4a7a2efcb3b6fd9a0a67c4fb5ec31
olig331 <ohg3@hotmail.co.uk>
2021-03-05 10:22:18 +0000

    big

--- server.js
+++ server.js
@@ -50,12 +50,12 @@ io.on("connection", socket => {
     });
 });
 
-if(process.env.PROD) {
-    app.use(express.static(path.join(__dirname, "./client/build")));
-    app.get('/', (req, res) => {
-        res.sendFile(path.join(__dirname, "./client/build/index.html"));
-    });
-}
+app.use(express.static(path.join(__dirname, "./client/build")));
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
 
 
 const port = process.env.PORT || 3001
